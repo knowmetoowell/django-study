2192ec8b2e2f046dc1392b23d9c51a680c30b738
knowmetoowell <kumct12@naver.com>
2022-01-11 22:11:56 +0900

    django-study part3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -17,7 +17,13 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-# Leave the rest of the views (detail, results, vote) unchanged
+    #render() 함수는 request 객체를 첫번째 인수로 받고, 
+    # 템플릿 이름을 두번째 인수로 받으며, 
+    # context 사전형 객체를 세전째 선택적(optional) 인수로 받습니다. 
+    # 인수로 지정된 context로 표현된 템플릿의 HttpResponse 객체가 반환됩니다.
+    # 모든 뷰에 적용한다면, 더 이상 loader와 HttpResponse를 임포트하지 않아도 됩니다. 
+    # (만약 detail, results, vote에서 stub 메소드를 가지고 있다면, HttpResponse를 유지해야 할 것입니다.)
+
 
 # def detail(request, question_id):
 #     try:
@@ -28,6 +34,9 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    #get_object_or_404() 함수는 Django 모델을 첫번째 인자로 받고, 
+    # 몇개의 키워드 인수를 모델 관리자의 get() 함수에 넘깁니다. 
+    # 만약 객체가 존재하지 않을 경우, Http404 예외가 발생합니다.
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
