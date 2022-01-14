ab4477e7214ecd15bd708efa7d93519d3b4adb3d
knowmetoowell <kumct12@naver.com>
2022-01-14 22:50:30 +0900

    part5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,7 @@ from django.http import Http404
 from django.views import generic
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     template = loader.get_template('polls/index.html')
@@ -51,12 +52,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
